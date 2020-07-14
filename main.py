f9edd1b7bebaf22442fb27fc3b0fcc59b804d049
Kidir Torain <kktorainnn@gmail.com>
2020-07-14 18:18:51 +0000

    Print

---
+++ main.py
@@ -0,0 +1,19 @@
+name = "Kidir Torain"
+
+print(name)
+
+name = "Frank Ocean"
+
+print(name)
+
+place = "Jamaica"
+
+print(place)
+
+hobby = "Dancing"
+
+print(hobby)
+
+date = "7/14/2020"
+
+print(date)
